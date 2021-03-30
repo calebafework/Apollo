fa17a233e4caa6c8467062bda633188f3a49bd99
nikkig124 <nikkig124@gmail.com>
2021-03-30 14:38:12 -0500

    remove commented code

--- app/models/emotion.js
+++ app/models/emotion.js
@@ -1,28 +1,3 @@
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class emotion extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//       models.emotion.belongsTo(models.comment)
-//     }
-//   };
-//   emotion.init({
-//     mood: DataTypes.STRING
-//   }, {
-//     sequelize,
-//     modelName: 'emotion',
-//   });
-//   return emotion;
-// };
-
 const Sequelize = require('sequelize');
 const db = require('../utils/database');
 
